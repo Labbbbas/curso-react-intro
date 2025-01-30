1acfec5019a7bb81b5c55ea27a63cc47cd375bc6
FernandoLabastida <labastidafernando@comunidad.unam.mx>
2025-01-29 20:36:29 -0600

    Reto: Migrar a React17, cambiando la versión de Toastify y el index.js de la app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// Con React 18
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+// Con React 17
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+
+const root = document.getElementById("root");
+render(<App />, root);
